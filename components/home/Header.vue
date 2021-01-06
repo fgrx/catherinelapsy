18664fef56b0e6c1c3c657c4c82679fda89d4289
fabien <fabien.grignoux@outlook.com>
2021-01-06 10:09:22 +0100

    home

---
+++ components/home/Header.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="overflow-hidden bg-gradient-to-r from-blue-700 to-blue-900">
+    <div class="flex">
+      <div class="flex-auto">
+        <img
+          class="ml-5 mt-10"
+          src="~/assets/catedetour.png"
+          alt="Portrait de Catherine la psy"
+        />
+      </div>
+      <div class="flex-auto">
+        <h1 class="text-5xl text-white">Catherine la Psy</h1>
+        <p class=""></p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+img {
+  filter: drop-shadow(30px 0px 40px rgb(71, 71, 71));
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,34 +1,33 @@
 <template>
-  <div class="container">
-    <div>
-      <Logo />
-      <h1 class="title">
-        catherinelapsy
-      </h1>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
+  <div>
+    <Header />
+    <div class="bg-grey">
+      <h2>Formations</h2>
     </div>
   </div>
 </template>
 
 <script>
-export default {}
+export default {
+  async asyncData({ $content }) {
+    const general = await $content("general").fetch();
+
+    return {
+      general,
+    };
+  },
+  computed: {
+    generalData() {
+      return this.general.general;
+    },
+    siteTitle() {
+      return this.general.general.siteTitle;
+    },
+    siteSubTitle() {
+      return this.general.general.siteSubtitle;
+    },
+  },
+};
 </script>
 
 <style>
@@ -37,42 +36,4 @@ export default {}
 @apply min-h-screen flex justify-center items-center text-center mx-auto;
 }
 */
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family:
-    'Quicksand',
-    'Source Sans Pro',
-    -apple-system,
-    BlinkMacSystemFont,
-    'Segoe UI',
-    Roboto,
-    'Helvetica Neue',
-    Arial,
-    sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
 </style>
