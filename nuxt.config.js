f6e243d644c54a2543c589b4c70dd20b9c8a8df2
fabien <fabien.grignoux@outlook.com>
2021-01-25 10:38:55 +0100

    test plugin for netlifyIdentity

--- nuxt.config.js
+++ nuxt.config.js
@@ -26,7 +26,11 @@ export default {
   css: [],
 
   // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
-  plugins: [],
+  plugins: [
+    {
+      src: "@/plugins/netlifyIdentity.js"
+    }
+  ],
 
   // Auto import components (https://go.nuxtjs.dev/config-components)
   components: true,
