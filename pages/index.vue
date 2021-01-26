7dd9c28d8410e2700ffe6fac920208200a1e3561
fabien <fabien.grignoux@outlook.com>
2021-01-26 08:40:18 +0100

    Add vue Hydration

--- pages/index.vue
+++ pages/index.vue
@@ -1,24 +1,38 @@
 <template>
   <div>
-    <section>
-      <TopHome />
-    </section>
+    <LazyHydrate when-visible>
+      <section>
+        <TopHome />
+      </section>
+    </LazyHydrate>
 
-    <section>
-      <Newsletter />
-    </section>
+    <LazyHydrate when-visible>
+      <section>
+        <Newsletter />
+      </section>
+    </LazyHydrate>
 
-    <section>
-      <Product :product="general.product" />
-    </section>
+    <LazyHydrate when-visible>
+      <section>
+        <Product :product="general.product" />
+      </section>
+    </LazyHydrate>
 
-    <section>
-      <SocialNetworks :socialNetworks="general.socialNetworks" />
-    </section>
+    <LazyHydrate when-visible>
+      <section>
+        <SocialNetworks :socialNetworks="general.socialNetworks" />
+      </section>
+    </LazyHydrate>
   </div>
 </template>
 
 <script>
+import LazyHydrate from "vue-lazy-hydration";
+import TopHome from "@/components/home/TopHome";
+import Newsletter from "@/components/home/Newsletter";
+import Product from "@/components/home/Product";
+import SocialNetworks from "@/components/home/SocialNetworks";
+
 export default {
   async asyncData({ $content }) {
     const general = await $content("general").fetch();
@@ -26,6 +40,12 @@ export default {
     return {
       general,
     };
+  },
+
+  components: {
+    LazyHydrate,
+    Product,
+    SocialNetworks,
   },
 
   head() {
