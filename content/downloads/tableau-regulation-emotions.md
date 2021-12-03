41ea84e765f7b04c62fec29fe39e1bbd11cd318b
Fabien <fab.grignoux@gmail.com>
2021-12-03 12:51:28 +0100

    Fix downloads

---
+++ content/downloads/tableau-regulation-emotions.md
@@ -0,0 +1,6 @@
+---
+title: Tableau de régulation des émotions
+url: tableau-regulation-des-emotions.pdf
+order: 1
+description: Ce tableau va vous permettre de trouver et d'analyser vos émotions pour pouvoir ensuite travailler dessus.
+---
--- pages/downloads/_slug.vue
+++ pages/downloads/_slug.vue
@@ -21,7 +21,9 @@
           <h2 v-if="file.title">
             {{ file.title }}
           </h2>
-          <p class="my-3" v-if="file.description">{{ file.description }}</p>
+          <p class="my-3" v-if="file.description">
+            Téléchargement du fichier "{{ file.title }}""
+          </p>
           <p class="my-3">
             Pour télécharger ce fichier, cliquez sur le bouton ci-dessous :
           </p>
@@ -60,8 +62,8 @@ export default {
     Newsletter,
   },
   async asyncData({ $content, params }) {
-    const downloads = await $content("downloads").fetch();
-    return { downloads };
+    const file = await $content("downloads", params.slug).fetch();
+    return { file };
   },
   data() {
     return {
@@ -76,20 +78,7 @@ export default {
       file: {},
     };
   },
-  async created() {
-    const result = this.downloads.files.filter(
-      (download) => download.id === this.fileSlug
-    );
 
-    this.file = result[0];
-    this.doc.description = `Téléchargement du fichier "${this.file.title}"`;
-  },
-
-  computed: {
-    fileSlug() {
-      return this.$route.params.slug;
-    },
-  },
   head() {
     return {
       title: "Téléchargement de fichiers",
