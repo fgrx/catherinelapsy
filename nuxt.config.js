789acbc96f3e6d5d3e118c88abc542a9a41eba2e
Fabien Grignoux <fabien.grignoux@outlook.com>
2022-02-23 12:42:06 +0100

    test build

--- nuxt.config.js
+++ nuxt.config.js
@@ -62,7 +62,7 @@ export default {
   modules: [
     "@nuxt/content",
     "@nuxt/image",
-    "nuxt-cookie-control",
+    //"nuxt-cookie-control",
     "@nuxtjs/pwa",
     "@nuxtjs/sitemap",
     [
@@ -141,28 +141,28 @@ export default {
   googleAnalytics: {
     id: "G-JFDNW9JFSX",
   },
-  cookies: {
-    necessary: [],
-    optional: [
-      {
-        name: "Google Analitycs",
-        identifier: "G-JFDNW9JFSX",
-        description:
-          "Nous utilisons l'outil Google Analytics à des fins statistiques afin d'améliorer nos services.",
+  // cookies: {
+  //   necessary: [],
+  //   optional: [
+  //     {
+  //       name: "Google Analitycs",
+  //       identifier: "G-JFDNW9JFSX",
+  //       description:
+  //         "Nous utilisons l'outil Google Analytics à des fins statistiques afin d'améliorer nos services.",
 
-        initialState: true,
-        src: "https://www.googletagmanager.com/gtag/js?id=G-JFDNW9JFSX",
-        async: true,
-        cookies: ["_ga", "_gat", "_gid"],
-        accepted: () => {
-          window.dataLayer = window.dataLayer || [];
-          window.dataLayer.push({
-            "gtm.start": new Date().getTime(),
-            event: "gtm.js",
-          });
-        },
-        declined: () => {},
-      },
-    ],
-  },
+  //       initialState: true,
+  //       src: "https://www.googletagmanager.com/gtag/js?id=G-JFDNW9JFSX",
+  //       async: true,
+  //       cookies: ["_ga", "_gat", "_gid"],
+  //       accepted: () => {
+  //         window.dataLayer = window.dataLayer || [];
+  //         window.dataLayer.push({
+  //           "gtm.start": new Date().getTime(),
+  //           event: "gtm.js",
+  //         });
+  //       },
+  //       declined: () => {},
+  //     },
+  //   ],
+  // },
 };
