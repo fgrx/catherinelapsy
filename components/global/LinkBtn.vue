cfa88fa217765da2e96333912ed628eca2193c52
Fabien Grignoux <fabien.grignoux@outlook.com>
2022-05-06 09:51:16 +0200

    Page ressources

--- components/global/LinkBtn.vue
+++ components/global/LinkBtn.vue
@@ -1,23 +1,36 @@
 <template>
-  <div class="text-center">
-    <a :href="url" :target="target">
-      <Btn class="">
+  <div :class="position">
+    <a v-if="url" :href="url" :target="target">
+      <Btn>
         <slot></slot>
       </Btn>
     </a>
+
+    <nuxt-link v-if="to" :to="url">
+      <Btn>
+        <slot></slot>
+      </Btn>
+    </nuxt-link>
   </div>
 </template>
 
 <script>
+import { stringify } from "querystring";
+
 export default {
   props: {
     url: {
       type: String,
       default: "",
     },
+    to: { type: String, default: "" },
     target: {
       type: String,
       default: "",
+    },
+    position: {
+      type: String,
+      default: "text-center",
     },
   },
 };
